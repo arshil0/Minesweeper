9e452d54e206ec979f581c45ee22688a6be9d6f2
arshil0 <arshil_aragelian@edu.aua.am>
2022-05-10 23:55:36 +0400

    Update Item.java

--- Items/Item.java
+++ Items/Item.java
@@ -11,22 +11,34 @@ import java.util.ArrayList;
 
 public class Item extends JLabel implements MouseListener {
 
+    private JDialog description = new JDialog();
+    private JLabel text = new JLabel();
+
     private static ArrayList<Item> allItems;
     private UI ongoingGame;
 
     public Item(UI game){
-        Class derivedClass = this.getClass();
-        if(!allItems.contains(derivedClass))
         this.ongoingGame = game;
+        description.add(text);
+        description.setLocationRelativeTo(this);
+        setDescription("<html>test<br/>tests<br/>aa<br/>bb<br/>cc</html>");
         setIcon(new ImageIcon(new ImageIcon("src/Minesweeper/UI/Sprites/placeholder.png").getImage().getScaledInstance(UI.FILESIZE/ Minesweeper.getHeight()/2,UI.FILESIZE/Minesweeper.getWidth()/2, Image.SCALE_DEFAULT)));
         addMouseListener(this);
     }
 
     private void description(){
         if(!Minesweeper.isLost()){
-            JDialog a = new JDialog();
-            a.setVisible(true);
+            description.setVisible(true);
         }
+    }
+
+    public void setDescription(String text){
+        int lines = text.split("<br/>").length;
+        System.out.println(lines);
+        this.text.setForeground(Color.BLUE);
+        description.setSize(400,(lines * 20) + 50);
+        description.revalidate();
+        this.text.setText(text);
     }
 
     @Override
@@ -51,6 +63,6 @@ public class Item extends JLabel implements MouseListener {
 
     @Override
     public void mouseExited(MouseEvent e) {
-
+        description.setVisible(false);
     }
 }
